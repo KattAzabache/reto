49e22082225c15f9d3d89b295f8c01cfa28af97e
katty-azabache <kattyazabachez@gmail.com>
2015-04-21 13:01:29 -0500

    faltan detalles

--- mi-script.js
+++ mi-script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+/*$(document).ready(function(){
 $(window).scroll(function(){
 var prueba = $(this).scrollTop();
 $("#katty").text(prueba);
@@ -9,4 +9,28 @@ else if (prueba < 658) {
 $(".navbar-default").removeClass("navegacion");
 }
 });
-});
+});*/
+$(document).ready(main);
+ 
+var contador = 1;
+ 
+function main () {
+	$('.menu_bar').click(function(){
+		if (contador == 1) {
+			$('nav').animate({
+				left: '0'
+			});
+			contador = 0;
+		} else {
+			contador = 1;
+			$('nav').animate({
+				left: '-100%'
+			});
+		}
+	});
+ 
+	// Mostramos y ocultamos submenus
+	$('.submenu').click(function(){
+		$(this).children('.children').slideToggle();
+	});
+}
